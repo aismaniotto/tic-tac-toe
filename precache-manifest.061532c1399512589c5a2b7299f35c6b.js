59b4f7efae3c719ef154f5accf21c787f00b7ecc
aismaniotto <aismaniotto@gmail.com>
2020-08-26 22:43:59 -0300

    Updates

---
+++ precache-manifest.061532c1399512589c5a2b7299f35c6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76bce24fe86ef07116c74cccc70969b5",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "5d39325c556829392d02",
+    "url": "/tic-tac-toe/static/css/main.a4ef488b.chunk.css"
+  },
+  {
+    "revision": "5885ac3b05bbd951f8cd",
+    "url": "/tic-tac-toe/static/js/2.792869cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tic-tac-toe/static/js/2.792869cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d39325c556829392d02",
+    "url": "/tic-tac-toe/static/js/main.5904371b.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
